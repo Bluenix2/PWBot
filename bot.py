efebbcec0b2e07cd43200a840066ed1427adef09
Bluenix <gabriel.nordholm@gmail.com>
2020-09-24 20:23:22 +0200

    Implement simple on_command_error

--- bot.py
+++ bot.py
@@ -2,6 +2,7 @@ import datetime
 import sys
 import traceback
 
+import discord
 from discord.ext import commands
 
 import config
@@ -35,6 +36,20 @@ class PWBot(commands.Bot):
             self.uptime = datetime.datetime.utcnow()
 
         print(f'Ready: {self.user} (ID: {self.user.id})')
+
+    async def on_command_error(self, ctx, error):
+        if isinstance(error, commands.NoPrivateMessage):
+            await ctx.author.send('This command cannot be used in private messages.')
+
+        elif isinstance(error, commands.DisabledCommand):
+            await ctx.author.send('Sorry, this command cannot be used at the moment.')
+
+        elif isinstance(error, commands.CommandInvokeError):
+            original = error.original
+            if not isinstance(original, discord.HTTPException):
+                print(f'Inside command {ctx.command.qualified_name}:', file=sys.stderr)
+                traceback.print_tb(original.__traceback__)
+                print(f'{original.__class__.__name__}: {original}', file=sys.stderr)
 
     async def process_commands(self, message):
         ctx = await self.get_context(message, cls=context.Context)
