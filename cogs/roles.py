5d24408bca2a92abf27ee95df17eee75ef101a1e
Bluenix <gabriel.nordholm@gmail.com>
2020-10-12 20:46:26 +0200

    Add initial reaction role embed command

--- cogs/roles.py
+++ cogs/roles.py
@@ -1,3 +1,4 @@
+import discord
 from discord.ext import commands
 
 
@@ -12,6 +13,50 @@ class Roles(commands.Cog):
             '🎟': 765263058515525633,
             '🏆': 765263032489738293
         }
+
+    @commands.command(name='sendrolemessage')
+    async def send_role_message(self, ctx):
+        await ctx.message.delete()
+
+        description = """To avoid pinging everyone we've created a few roles to ping instead.
+
+        React to this message to assign the appropriate role.
+        If you have any questions feel free to ping a Community Manager
+        """
+        embed = discord.Embed(
+            title='Role Management',
+            description=description,
+            colour=discord.Colour.greyple()
+        )
+
+        embed.add_field(
+            name='\N{CHEERING MEGAPHONE} Announcements',
+            value='Will get pinged in <#489825441075429378> when the game is updated or the developers have important information to share.',
+            inline=False
+        )
+        embed.add_field(
+            name='\N{ADMISSION TICKETS} Events',
+            value='Pinged in <#651109293663191061> or <#489825441075429378> when a new event is created, or event details have been released/updated.',
+            inline=False
+        )
+        embed.add_field(
+            name='\N{TROPHY} Tournaments',
+            value='Pinged in <#647955879844380682> when new community-run tournaments are hosted or information is released/updated.',
+            inline=False
+        )
+        embed.add_field(
+            name='\N{GLOBE WITH MERIDIANS} Community Translations',
+            value='We are looking to let active community members helps us translate the game.\n **If you want to help please contact a Community Manager**.',
+            inline=False
+        )
+
+        message = await ctx.send(embed=embed)
+
+        await message.add_reaction('\N{CHEERING MEGAPHONE}')
+        await message.add_reaction('\N{ADMISSION TICKETS}')
+        await message.add_reaction('\N{TROPHY}')
+
+        self.bot.settings.reaction_message = message.id
 
     @commands.Cog.listener()
     async def on_raw_reaction_add(self, payload):
