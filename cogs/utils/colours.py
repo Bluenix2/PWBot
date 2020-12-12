59e262fa93befd7d07898e82091319f411a1cdea
Bluenix <gabriel.nordholm@gmail.com>
2020-12-13 00:53:19 +0100

    Fix lazy refactoring to classmethods

--- cogs/utils/colours.py
+++ cogs/utils/colours.py
@@ -11,17 +11,17 @@ class Colour(discord.Colour):
         return cls(0x63C1B1)
 
     @classmethod
-    def light_blue():
-        return discord.Colour(0x65A3E6)
+    def light_blue(cls):
+        return cls(0x65A3E6)
 
     @classmethod
-    def red():
-        return discord.Colour(0xA62A2A)
+    def red(cls):
+        return cls(0xA62A2A)
 
     @classmethod
-    def unvaulted_red():
-        return discord.Color(0xE03D51)
+    def unvaulted_red(cls):
+        return cls(0xE03D51)
 
     @classmethod
-    def apricot():
-        return discord.Colour(0xE16859)
+    def apricot(cls):
+        return cls(0xE16859)
